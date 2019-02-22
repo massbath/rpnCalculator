921098434048a89fc1bcaf79846943519b599d7e
massbath <clement.vassant@gmail.com>
2019-02-22 20:16:38 +0100

    A expression with numbers separate with whitespace should give the same output

--- src/test/java/RnpTest.java
+++ src/test/java/RnpTest.java
@@ -12,10 +12,17 @@ public class RnpTest {
 
     @Test
     @Parameters({"1", "2", "B"})
-    public void simple_digit_should_give_simple_digit(String simpleDigit) {
+    public void a_expression_with_simple_digit_should_give_simple_digit(String simpleDigit) {
         String resultat = Rpn.calculate(simpleDigit);
 
         assertThat(resultat).isEqualTo(simpleDigit);
+    }
+
+    @Test
+    @Parameters({"1 2", "3 4"})
+    public void a_expression_with_simple_number_with_whitespace_should_give_the_identical_output(String expression) {
+        String resultat = Rpn.calculate(expression);
+        assertThat(resultat).isEqualTo(expression);
     }
 
 
