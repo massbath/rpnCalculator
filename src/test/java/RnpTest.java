53a3de19eb12da109fb3b47638890584d1f734b7
massbath <clement.vassant@gmail.com>
2019-02-25 16:31:05 +0100

    refactoring migrate test to property based test

--- src/test/java/RnpTest.java
+++ src/test/java/RnpTest.java
@@ -18,62 +18,8 @@ public class RnpTest {
         assertThat(resultat).isEqualTo(simpleDigit);
     }
 
-    @Test
-    @Parameters({"1 2", "3 4"})
-    public void a_expression_with_simple_number_with_whitespace_should_give_the_identical_output(String expression) {
-        String resultat = Rpn.calculate(expression);
-        assertThat(resultat).isEqualTo(expression);
-    }
 
-    @Test
-    @Parameters({"1 1 +,2", "2 1 +,3", "3 1 +,4", "1 3 +,4", "1 6 +,7", "10 10 +,20"})
-    public void a_expression_with_simple_numbers_and_plus_operator_should_give_the_sum_of_both(String expression, String resultatExpected) {
 
-        String resultat = Rpn.calculate(expression);
-        assertThat(resultat).isEqualTo(resultatExpected);
-
-    }
-
-    @Test
-    @Parameters({
-            "1 1 -,0",
-            "1 2 -,-1",
-            "1 -2 -,3"
-    })
-    public void a_expression_with_simple_numbers_and_minus_operator_should_give_the_sum_of_both(String expression, String resultatExpected) {
-
-        String resultat = Rpn.calculate(expression);
-        assertThat(resultat).isEqualTo(resultatExpected);
-
-    }
-
-    @Test
-    @Parameters({"20 5 /,4", "1 1 /,1", "3 4 /,0"})
-    public void a_expression_with_simple_numbers_and_divide_operator_should_give_the_substract_of_both(String expression, String resultatExpected) {
-
-        String resultat = Rpn.calculate(expression);
-        assertThat(resultat).isEqualTo(resultatExpected);
-
-    }
-
-    @Test
-    @Parameters({
-            "20 5 *,100",
-            "1 1 *,1",
-            "3 4 *,12"
-    })
-    public void a_expression_with_simple_numbers_and_multiply_operator_should_give_the_multiplev_of_both(String expression, String resultatExpected) {
-
-        String resultat = Rpn.calculate(expression);
-        assertThat(resultat).isEqualTo(resultatExpected);
-
-    }
-
-    @Test
-    public void a_expression_with_number_and_divide_operator_which_is_invalid_should_give_the_same_expressions() {
-        String resultat = Rpn.calculate("2 0 /");
-        assertThat(resultat).isEqualTo("2 0 /");
-    }
 
     @Test
     @Parameters({
--- src/test/java/RpnPBTTest.java
+++ src/test/java/RpnPBTTest.java
@@ -1,19 +1,81 @@
 import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import domain.Rpn;
-import org.assertj.core.api.Assertions;
 import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(JUnitQuickcheck.class)
 public class RpnPBTTest {
 
     @Property
+    public void a_expression_with_simple_digit_should_give_simple_digit(String randomString) {
+        String resultat = Rpn.calculate(randomString);
+
+        assertThat(resultat).isEqualTo(randomString);
+    }
+
+    @Property
+    public void a_expression_with_simple_number_with_whitespace_should_give_the_identical_output(Integer firstNumber, Integer secondNumber) {
+        final String expression = firstNumber + " " + secondNumber;
+        String resultat = Rpn.calculate(expression);
+        assertThat(resultat).isEqualTo(expression);
+    }
+
+    @Property
     public void given_2_random_numbers_and_plus_operator_should_give_the_sum_of_both(Integer firstOperand, Integer secondOperand) {
-        String expression = firstOperand + " " + secondOperand + " +";
+        String expression = makeExpression(firstOperand, secondOperand, "+");
         String resultatExpected = String.valueOf(firstOperand + secondOperand);
 
         String resultat = Rpn.calculate(expression);
 
-        Assertions.assertThat(resultat).isEqualTo(resultatExpected);
+        assertThat(resultat).isEqualTo(resultatExpected);
     }
+
+    @Property
+    public void given_2_random_numbers_and_minus_operator_should_give_the_subtract_of_first_operand_by_second_operand(Integer firstOperand, Integer secondOperand) {
+        String expression = makeExpression(firstOperand, secondOperand, "-");
+        String resultatExpected = String.valueOf(firstOperand - secondOperand);
+
+        String resultat = Rpn.calculate(expression);
+
+        assertThat(resultat).isEqualTo(resultatExpected);
+    }
+
+    @Property
+    public void given_2_random_numbers_and_multiply_operator_should_give_the_multiplication_of_both(Integer firstOperand, Integer secondOperand) {
+        String expression = makeExpression(firstOperand, secondOperand, "*");
+        String resultatExpected = String.valueOf(firstOperand * secondOperand);
+
+        String resultat = Rpn.calculate(expression);
+
+        assertThat(resultat).isEqualTo(resultatExpected);
+    }
+
+    @Property
+    public void given_2_random_numbers_and_division_operator_should_give_the_division_of_first_operand_by_second_operand(Integer firstOperand, @InRange(minInt = 1) Integer secondOperand) {
+        String expression = makeExpression(firstOperand, secondOperand, "/");
+        String resultatExpected = String.valueOf(firstOperand / secondOperand);
+
+        String resultat = Rpn.calculate(expression);
+
+        assertThat(resultat).isEqualTo(resultatExpected);
+    }
+
+    @Property
+    public void given_1_random_number_a_null_number_and_division_operator_should_give_the_division_of_first_operand_by_second_operand(Integer firstOperand) {
+        String expression = makeExpression(firstOperand, 0, "/");
+        String resultatExpected = firstOperand + " 0 /";
+
+        String resultat = Rpn.calculate(expression);
+
+        assertThat(resultat).isEqualTo(resultatExpected);
+    }
+
+    private String makeExpression(Integer firstOperand, Integer secondOperand, String operator) {
+        return firstOperand + " " + secondOperand + " " + operator;
+    }
+
+
 }
